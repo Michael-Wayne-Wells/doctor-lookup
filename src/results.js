2e4caf64ebf2c0aab616b2cd1af6180a591024ec
Michael Wells <mwells1286@gmail.com>
2019-11-22 13:43:43 -0800

    add css styles to form and refine results from doctor.

--- src/results.js
+++ src/results.js
@@ -15,27 +15,20 @@ export class SearchResults {
         }else{
           return "no";
         }
-      }
+      };
       let website = () => {
         if(doc.practices[0].website === undefined) {
           return "no website listed";
         } else {
           return doc.practices[0].website;
         }
-      }
+      };
       let firstName = doc.profile.first_name;
       let lastName = doc.profile.last_name;
       let address = doc.practices[0].visit_address.street + " " + doc.practices[0].visit_address.city + ", " + doc.practices[0].visit_address.state + " " + doc.practices[0].visit_address.zip;
       let phone = doc.practices[0].phones[0].number;
 
-      $('ul#search-results').append("<li>" + firstName + " " +
-      lastName +
-      "</li>" + "<ul>" + "<li>" + address +
-      "</li>" + "<li>" +
-      phone +
-      "</li>" +'<li>' +
-      website() + "</li>" +
-      "<li> Accepting new patients:" + acceptNew() + "</li>" + "</ul>" );
+      $('ul#search-results').append("<li>Name: " + firstName + " " + lastName + "</li>" + "<ul>" + "<li>Address: " + address + "</li>" + "<li> Phone: " + phone + "</li>" + "<li> Website: " + website() + "</li>" + "<li> Accepting new patients: " + acceptNew() + "</li>" + "</ul>" );
 
     });
   }
