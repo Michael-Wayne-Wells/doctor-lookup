b59e9944c8b1e8a02a4ed7a161a4e7d6897d6bbc
Michael Wells <mwells1286@gmail.com>
2019-11-22 12:15:45 -0800

    display additional results on doctors based on users search.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import {DoctorSearch} from './api-call.js';
-import {SearchResults} from './results.js'
+import {SearchResults} from './results.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,12 +15,12 @@ $(document).ready(function() {
     (async () => {
       let doctorSearch = new DoctorSearch(userSymptom, docName);
       const docResults = await doctorSearch.getDoctors();
-      let newResults = new SearchResults(docResults)
-      newResults.userResults(docResults);
+      let newResults = new SearchResults(docResults);
+      newResults.userResults();
 
     })();
 
 
-  })
+  });
 
 });
--- src/results.js
+++ src/results.js
@@ -2,18 +2,16 @@ import $ from 'jquery';
 
 
 export class SearchResults {
-  constructor() {
-    this.result = [];
-
+  constructor(docResults) {
+    this.result = docResults;
   }
 
-  userResults(docResults) {
-  let docArray = docResults.data
-  docArray.forEach(function(doc) {
-    $('ul#search-results').append("<li>" + doc.profile.first_name + " " + doc.profile.last_name + "</li>")
-
-  })
-
-
-}
+  userResults() {
+    let docArray = this.result.data;
+    docArray.forEach(function(doc) {
+      $('ul#search-results').append("<li>" + doc.profile.first_name + " " + doc.profile.last_name + "</li>" +
+      "<ul>" + "<li>" + doc.practices[0].visit_address.street + " " + doc.practices[0].visit_address.city + ", " + doc.practices[0].visit_address.state + " " + doc.practices[0].visit_address.zip + "</li>" +
+      "<li>" + doc.practices[0].phones[0].number + "</li>"   + "</ul>" );
+    });
+  }
 }
