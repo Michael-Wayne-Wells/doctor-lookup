36ed616f7735c2da0c91352fad45c1a6260c94ee
Michael Wells <mwells1286@gmail.com>
2019-11-22 16:19:28 -0800

    clean code.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
 import {DoctorSearch} from './api-call.js';
 import {SearchResults} from './results.js';
 import $ from 'jquery';
@@ -9,8 +10,8 @@ $(document).ready(function() {
 
   let locationCheck = new DoctorSearch();
   (async () => {
-  await locationCheck.getLocation();
-  $('#city').text(locationCheck.location[2])
+    await locationCheck.getLocation();
+    $('#city').text(locationCheck.location[2])
   })();
 
   $('form#user-input').submit(function(event) {
@@ -27,9 +28,9 @@ $(document).ready(function() {
       let doctorSearch = new DoctorSearch(userSymptom, docName, radius);
       await doctorSearch.getLocation();
       let docResults = await doctorSearch.getDoctors();
-    if(doctorSearch.error == true) {
-let error = await doctorSearch.getDoctors();
-  $('#no-results').text(error)
+      if(doctorSearch.error == true) {
+        let error = await doctorSearch.getDoctors();
+        $('#no-results').text(error)
 
       }
       let newResults = new SearchResults(docResults);
